783f799cf822fab531cf4b4a5d416b4dfe2ba186
emre <edursun@sogyo.nl>
2023-06-04 01:36:48 +0200

    All the conflicts inside the Test_Branch.java file are resolved!

--- src/main/java/org/example/Test_Branch.java
+++ src/main/java/org/example/Test_Branch.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class Test_Branch {
+    public static void main(String[] args) {
+        System.out.println("All the conflicts are resolved, added the modified files!");
+    }
 }
